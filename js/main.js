771f023cfafc9199b9abb939252d53e6b86dbc4c
Alister A. Fenix <alister.fenix@way-find.com>
2024-02-12 19:50:12 -0800

    createMap function enabled, to read getData() method

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-// Add all scripts to the JS folder
+// The following code is adapted from the Leaflet Geojson Tutorial by Leaflet.js (https://leafletjs.com/examples/geojson/)
+
+/* Map of GeoJSON data from MegaCities.geojson */
+//declare map var in global scope
+var map;
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    map = L.map('map-container').setView([45.5182, -122.6684], 13);
+
+    // Adds a tile layer to the map using Stadia Maps' Alidade Smooth tiles for terrain visualization.
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+        minZoom: 0,
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	    ext: 'png'
+    }).addTo(map)
+
+     // Initiates the retrieval and display of GeoJSON data by calling the getData function.
+    getData();
+};
